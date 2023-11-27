cd11576786a67666cf07b3a5da759695dc5f0443
Mckenzie Regalado <iammckenzie1227@gmail.com>
2023-11-27 20:43:30 +0800

    set the optimization level to -03.

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-gcc -Wall -O0 -std=c11 fourier1.c realfft.c main.c -o run -I include/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
+gcc -Wall -O3 -std=c11 fourier1.c realfft.c main.c -o run -I include/ -L lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
